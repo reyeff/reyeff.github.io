df2ba220fd55c5f5584efe74fc648a6f7c61ee24
Reynaldi <36196312+reyeff@users.noreply.github.com>
2022-03-01 00:03:14 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -79,7 +79,7 @@ module.exports = {
         snippetVersion : 6
     },
     themeConfig: {
-        default: 'light',
+        default: 'dark',
 
         // Hides the switch in the navbar
         // Useful if you want to support a single color mode
